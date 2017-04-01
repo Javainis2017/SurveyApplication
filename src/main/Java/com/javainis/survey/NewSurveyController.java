4623553372cd65ab5d8a1c56db51475fd6855c41
Simonas <simsas18@gmail.com>
2017-04-01 20:42:35 +0300

    Added survey entity, dao and controller

---
+++ src/main/Java/com/javainis/survey/NewSurveyController.java
@@ -0,0 +1,16 @@
+package com.javainis.survey;
+
+public class NewSurveyController {
+
+    public void addQuestion(){
+
+    }
+
+    public void removeQuestion(){
+
+    }
+
+    public void createSurvey(){
+
+    }
+}
---
+++ src/main/Java/com/javainis/survey/Survey.java
@@ -0,0 +1,51 @@
+package com.javainis.survey;
+
+import com.javainis.user_management.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Table(name = "survey")
+@NamedQueries({
+    @NamedQuery(name = "Survey.findAll", query = "SELECT s FROM Survey s"),
+    @NamedQuery(name = "Survey.findById", query = "SELECT s FROM Survey s WHERE s.id = :id"),
+    @NamedQuery(name = "Survey.findByUrl", query = "SELECT s FROM Survey s WHERE s.url = :url"),
+    @NamedQuery(name = "Survey.findByAuthorId", query = "SELECT s FROM Survey s WHERE s.author.userID = :authorId")
+})
+@Getter
+@Setter
+
+public class Survey {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    @Size(max = 80)
+    private String name;
+
+    @Column(name = "description")
+    @Size(max = 500)
+    private String description;
+
+    @Column(name = "url")
+    @Size(max = 32, min = 32)
+    private String url;
+
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @ManyToOne
+    private User author;
+
+    @Version
+    @Column(name = "opt_lock_version")
+    private Integer optLockVersion;
+
+    /*TODO questions
+    @OneToMany(mappedBy = "survey")
+    private List<Question> questions;*/
+}
