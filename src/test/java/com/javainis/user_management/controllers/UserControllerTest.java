919adfd9f6a85ebb77b4c2f030bf87bd3f2c4d4a
coderplug <dovydas.jurenkovas@mif.stud.vu.lt>
2017-04-13 21:09:21 +0300

    Test case with Mockito.

--- src/test/java/com/javainis/user_management/controllers/UserControllerTest.java
+++ src/test/java/com/javainis/user_management/controllers/UserControllerTest.java
@@ -1,24 +1,46 @@
 package com.javainis.user_management.controllers;
 
 import com.javainis.user_management.entities.User;
+import com.javainis.utility.HashGenerator;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
 import javax.inject.Inject;
 
 import static org.junit.Assert.*;
-
-
+@RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
 
+    @InjectMocks
+    private UserController userController;
+    @Mock
+    private HashGenerator hashGenerator;
 
-    private UserController userController = new UserController();
+    // = new UserController();
+
+    @Before
+    public void setUp() throws Exception
+    {
+        userController = new UserController();
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void login() throws Exception {
-        userController.getUser().setEmail("a@a.lt");
-        userController.setPasswordHash("a");
-        userController.login();
-        assertEquals("a@a.lt", userController.getUser().getEmail());
+        //hashGenerator.generatePasswordHash("a");
 
+        //userController.getUser().setEmail("a@a.lt");
+        String message = hashGenerator.generatePasswordHash("a");
+        //userController.setPasswordHash("a");
+        userController.getUser().setPasswordHash(message);
+        //userController.login();
+        userController.setPasswordHash("a");
+        assertEquals(message, userController.getUser().getPasswordHash());
     }
 
 }
