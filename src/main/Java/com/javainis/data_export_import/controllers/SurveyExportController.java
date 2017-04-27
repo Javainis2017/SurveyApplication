aad29ce2e3f726e5b1fa7d23123c0375110cee7c
Laurynas <laurynas126@gmail.com>
2017-04-27 11:45:55 +0300

    Create XLSXDataExporter

--- src/main/Java/com/javainis/data_export_import/controllers/SurveyExportController.java
+++ src/main/Java/com/javainis/data_export_import/controllers/SurveyExportController.java
@@ -1,5 +1,6 @@
 package com.javainis.data_export_import.controllers;
 
+import com.javainis.data_export_import.data_converters.XLSXDataExporter;
 import com.javainis.data_export_import.interfaces.DataExporter;
 import com.javainis.survey.dao.SurveyDAO;
 import com.javainis.survey.dao.SurveyResultDAO;
@@ -10,8 +11,12 @@ import lombok.Setter;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
+import javax.transaction.Transactional;
+import java.io.File;
 import java.util.List;
 
+@Named
 @RequestScoped
 public class SurveyExportController {
 
@@ -32,8 +37,11 @@ public class SurveyExportController {
     @Setter
     private List<Answer> surveyAnswers;
 
-    public void  exportSurvey()
+    @Transactional
+    public void exportSurvey()
     {
+        Survey test1 = surveyDAO.getAll().get(0);
+        exporter.exportSurvey(test1, new File("survey.xlsx"));
 
     }
 
--- src/main/Java/com/javainis/data_export_import/data_converters/XLSXDataExporter.java
+++ src/main/Java/com/javainis/data_export_import/data_converters/XLSXDataExporter.java
@@ -1,25 +1,91 @@
 package com.javainis.data_export_import.data_converters;
 
 import com.javainis.data_export_import.interfaces.DataExporter;
-import com.javainis.survey.entities.Answer;
-import com.javainis.survey.entities.Survey;
+import com.javainis.survey.entities.*;
+
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.primefaces.model.StreamedContent;
 
 import javax.enterprise.context.ApplicationScoped;
-import java.io.File;
+import javax.enterprise.context.RequestScoped;
+import java.io.*;
 import java.util.List;
 
 @ApplicationScoped
-public class XLSXDataExporter implements DataExporter {
+public class XLSXDataExporter implements DataExporter{
 
-    private File file;
+    private void ChoiceQuestion(XSSFRow row, List<Choice> choices)
+    {
+        int cell = 3;
+        for(Choice c: choices)
+        {
+            row.createCell(cell++).setCellValue(c.getText());
+        }
+    }
 
-    @Override
-    public void exportSurvey(Survey survey, File destination) {
-
+    private void IntervalQuestion(XSSFRow row, int min, int max)
+    {
+        row.createCell(3).setCellValue(min);
+        row.createCell(4).setCellValue(max);
     }
 
     @Override
-    public void exportAnswers(List<Answer> answers, File destination) {
+    public void exportSurvey(Survey survey, File destination)
+    {
+        System.out.println("Exporting survey");
+        XSSFWorkbook wb = new XSSFWorkbook();
+        XSSFSheet surveySheet = wb.createSheet("Survey");
+        XSSFRow statusRow = surveySheet.createRow(0);
+        statusRow.createCell(0).setCellValue("$questionNumber");
+        statusRow.createCell(1).setCellValue("$question");
+        statusRow.createCell(2).setCellValue("$questionType");
+        statusRow.createCell(3).setCellValue("$optionsList");
+        List<Question> surveyQuestions = survey.getQuestions();
+        for(int i = 0; i < surveyQuestions.size(); i++)
+        {
+            Question question = surveyQuestions.get(i);
+            XSSFRow row = surveySheet.createRow(i+1);
+            row.createCell(0).setCellValue(i+1);
+            row.createCell(1).setCellValue(question.getText());
+            if (question instanceof MultipleChoiceQuestion)
+            {
+                row.createCell(2).setCellValue("MULTIPLECHOICE");
+                ChoiceQuestion(row, ((MultipleChoiceQuestion) question).getChoices());
+            }
+            else if (question instanceof SingleChoiceQuestion)
+            {
+                row.createCell(2).setCellValue("CHECKBOX");
+                ChoiceQuestion(row, ((SingleChoiceQuestion) question).getChoices());
+            }
+            else if (question instanceof IntervalQuestion)
+            {
+                row.createCell(2).setCellValue("SCALE");
+                IntervalQuestion iq = (IntervalQuestion) question;
+                IntervalQuestion(row, iq.getMinValue(), iq.getMaxValue());
+            }
+            else{
+                row.createCell(2).setCellValue("TEXT");
+            }
+        }
+        try {
+            FileOutputStream fileOut = new FileOutputStream(destination);
+            wb.write(fileOut);
+            fileOut.close();
+            System.out.println("Export finished");
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void exportAnswers(List<Answer> answers, File destination)
+    {
 
     }
+
 }
