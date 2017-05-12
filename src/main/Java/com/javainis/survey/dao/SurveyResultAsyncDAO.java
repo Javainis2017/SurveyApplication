f605fc1ec2d666016688a6490b351f3ec6feab1e
Laurynas <laurynas126@gmail.com>
2017-05-12 14:59:33 +0300

    Update theme

---
+++ src/main/Java/com/javainis/survey/dao/SurveyResultAsyncDAO.java
@@ -0,0 +1,29 @@
+package com.javainis.survey.dao;
+
+import com.javainis.Async;
+import com.javainis.survey.entities.SurveyResult;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+/**
+ * Created by Laurynas on 2017-05-10.
+ */
+@Dependent
+public class SurveyResultAsyncDAO {
+
+    @Inject @Async
+    private EntityManager manager;
+
+    public void create(SurveyResult surveyResult) {
+        manager.persist(surveyResult);
+    }
+
+    public List<SurveyResult> getResultsBySurveyId(Long surveyId){
+        return manager.createNamedQuery("SurveyResult.findBySurveyId", SurveyResult.class).setParameter("surveyId", surveyId).getResultList();
+    }
+}
