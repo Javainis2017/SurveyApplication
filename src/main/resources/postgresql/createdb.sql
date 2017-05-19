0f7f0ddfcb28fc138c25d943c6bd7104ccad1302
Simonas <simsas18@gmail.com>
2017-05-19 19:27:39 +0300

    Fix surveyResult url

--- src/main/resources/postgresql/createdb.sql
+++ src/main/resources/postgresql/createdb.sql
@@ -81,8 +81,8 @@ CREATE TABLE IF NOT EXISTS survey_result
 (
 	id 								BIGSERIAL PRIMARY KEY,
 	survey_id					BIGINT NOT NULL,
-	url								CHAR(32) NOT NULL,
-	complete					BOOLEAN DEFAULT FALSE,
+	url								CHAR(32),
+	complete					BOOLEAN DEFAULT TRUE,
 	opt_lock_version INTEGER,
 	FOREIGN KEY (survey_id) REFERENCES survey(id)
 );
