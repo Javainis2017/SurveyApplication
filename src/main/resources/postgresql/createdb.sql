773d94d3e0f5229c886d1471fe91b0c2cb1215d4
Simonas <simsas18@gmail.com>
2017-05-19 15:26:06 +0300

    SQL cleanup

--- src/main/resources/postgresql/createdb.sql
+++ src/main/resources/postgresql/createdb.sql
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS survey
 	url 			CHAR(32) NOT NULL,
 	expiration_time TIMESTAMP,
 	user_id 	BIGINT NOT NULL,
-	public 		BOOLEAN,
+	public 		BOOLEAN DEFAULT TRUE,
 	opt_lock_version INTEGER,
 	FOREIGN KEY (user_id) REFERENCES app_user(id)
 );
@@ -75,8 +75,6 @@ CREATE TABLE IF NOT EXISTS interval_question
 	id 					BIGSERIAL PRIMARY KEY,
 	min_value 	INTEGER,
 	max_value		INTEGER,
-	question_id BIGINT NOT NULL,
-	FOREIGN KEY (question_id) REFERENCES question(id)
 );
 
 CREATE TABLE IF NOT EXISTS survey_result
@@ -134,19 +132,6 @@ CREATE TABLE IF NOT EXISTS single_choice_question
 	FOREIGN KEY (id) REFERENCES question(id)
 );
 
-ALTER TABLE question DROP CONSTRAINT IF EXISTS question_type_id_fkey;
-DROP TABLE IF EXISTS question_type;
-
-ALTER TABLE IF EXISTS interval_choice RENAME TO interval_question;
-
-ALTER TABLE interval_question DROP CONSTRAINT IF EXISTS interval_choice_question_id_fkey;
-ALTER TABLE interval_question DROP COLUMN IF EXISTS question_id;
-
-ALTER TABLE answer ADD COLUMN IF NOT EXISTS answer_type CHAR(1);
-ALTER TABLE answer ADD COLUMN IF NOT EXISTS number_answer INTEGER;
-ALTER TABLE answer DROP COLUMN IF EXISTS answer;
-ALTER TABLE answer ADD COLUMN IF NOT EXISTS text_answer VARCHAR(1000);
-
 CREATE TABLE IF NOT EXISTS answer_choice
 (
 	answer_id BIGINT NOT NULL,
@@ -155,30 +140,13 @@ CREATE TABLE IF NOT EXISTS answer_choice
 	FOREIGN KEY (choice_id) REFERENCES choice(id)
 );
 
-ALTER TABLE answer ADD COLUMN IF NOT EXISTS choice_id BIGINT;
-ALTER TABLE answer DROP CONSTRAINT IF EXISTS choice_id_fkey;
-ALTER TABLE answer ADD FOREIGN KEY (choice_id) REFERENCES choice(id);
-
-ALTER TABLE mail_expiration DROP COLUMN IF EXISTS expiration_date;
-ALTER TABLE mail_expiration ADD COLUMN IF NOT EXISTS expiration_date TIMESTAMP;
-
-ALTER TABLE mail_expiration DROP COLUMN IF EXISTS user_id;
-ALTER TABLE mail_expiration ADD COLUMN IF NOT EXISTS user_id BIGINT;
-
-ALTER TABLE survey ADD COLUMN IF NOT EXISTS expiration_time TIMESTAMP;
-ALTER TABLE survey ADD COLUMN IF NOT EXISTS public	BOOLEAN DEFAULT TRUE;
-
-ALTER TABLE survey_result ADD COLUMN IF NOT EXISTS url CHAR(32);
-ALTER TABLE survey_result ADD COLUMN IF NOT EXISTS complete BOOLEAN;
-
 CREATE TABLE IF NOT EXISTS logs
 (
 	id BIGSERIAL PRIMARY KEY,
 	user_name		VARCHAR(80) NOT NULL,
+	user_email VARCHAR(50),
 	survey_name  VARCHAR(80) NOT NULL,
 	rights	VARCHAR(10),
 	time TIMESTAMP,
 	class_method VARCHAR(100)
 );
-
-ALTER TABLE logs ADD COLUMN IF NOT EXISTS user_email VARCHAR(50);
